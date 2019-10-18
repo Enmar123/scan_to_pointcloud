89d3807889e81d10359eaa4817df30c0f1b4b8cf
Enmar123 <jsoto103@fiu.edu>
2019-10-17 21:38:15 -0400

    fixed mp: 1cpu only

--- src/pc2_to_file.py
+++ src/pc2_to_file.py
@@ -6,18 +6,20 @@ Tested up to 20000 pts at 1hz
 fails at 20000 pts at 5hz
 """
 import rospy
+#from roslib import message
 
 from sensor_msgs.msg import PointCloud2
 from std_msgs.msg import Empty
 import sensor_msgs.point_cloud2
 import tf
 
+import copy
 import threading
 import struct
 import in_place
 import numpy as np
 from datetime import datetime
-from multiprocessing import Process, cpu_count
+from multiprocessing import Process, Manager, cpu_count
     
 def pc2_read_data(msg):
     points = []
@@ -29,9 +31,10 @@ def pc2_read_data(msg):
     return points
     
 def pc2_to_pts(msg):
+    #print("new =", msg.data[0:20])
     pts = pc2_read_data(msg)     # Extracts field data based on dtype
     pts = convert_rgb(pts, msg)  # Unpacks rgb field if it is present
-    print pts[0]    
+    #print pts[0]    
     return pts
     
 ### RGB section ### ----------------------------------------------------------
@@ -157,21 +160,21 @@ def pts_tf_threader(n_threads, trans, quat, pts):
     pts = [pt for points in ptss for pt in points]  # corrected order?
     return pts
     
-def pts_tf_multiprocess(n_threads, trans, quat, pts):
-    ptss= [None]*n_threads
+def pts_tf_multiprocess(n_process, trans, quat, pts):
+    ptss= [None]*n_process
     # figure out how to divide up the points
     n_pts = len(pts)  
-    l = n_pts/n_threads                 # len of typical thread
-    ln = n_pts - l*(n_threads-1)        # len of longer thread
+    l = n_pts/n_process                 # len of typical thread
+    ln = n_pts - l*(n_process-1)        # len of longer thread
     # dividee up the points
-    for i in range(n_threads):
+    for i in range(n_process):
         if i == 0:
             ptss[i] = pts[0:ln]
         else:
             ptss[i] = pts[ln + (i-1)*l:ln + i*l]
     
     processes = list()
-    for index in range(n_threads):
+    for index in range(n_process):
         x = Process(target=thread_function, args=(ptss, index, trans, quat))
         processes.append(x)
         x.start()
@@ -180,6 +183,54 @@ def pts_tf_multiprocess(n_threads, trans, quat, pts):
         process.join()
         
     pts = [pt for points in ptss for pt in points]  # corrected order?
+    return pts
+    
+def mp_function(ptss, index, msg, tf):
+    trans, quat = tf
+    pts = pc2_to_pts(msg)                         # Extract xyz and color data
+    pts = transform_points(trans, quat, pts)      # Transform position data
+    ptss[index] = pts                             # Store transformed points
+    #print("output =", pts )
+    
+def pc2_multiprocess(n_process, msg, tf):
+    trans, quat = tf
+    manager = Manager()
+    ptss = manager.list(range(n_process))                                                      # holder for multiple list-of-points
+    # figure out how to divide up the points
+    n_pts = int(msg.width/msg.point_step)
+    l = n_pts/n_process                                                         # len of typical thread
+    ln = n_pts - l*(n_process-1)                                                # len of longest thread
+    # Create empty copies of the message and assing them to a list
+#    data = msg.data                                                             # set data aside for later
+#    msg.data = []                                                               # set original message data as empty
+    msgs = [None]*n_process                                                     # Holder for multiple messages
+#    for index in range(n_process):
+#        msgs[index] = copy.deepcopy(msg)
+    # Divide and assign the data to each message in the list
+    for i in range(n_process):
+        if i == 0:
+#            data_chunk = data[0 : ln*msg.point_step]
+#            msgs[i].data = data_chunk 
+#            msgs[i].width = len(data_chunk)
+            msgs[i] = msg
+        else:
+            data_chunk = data[(ln + (i-1)*l)*msg.point_step : (ln + i*l)*msg.point_step]
+            msgs[i].data = data_chunk
+            msgs[i].width = len(data_chunk)
+            
+    # Start multiprocessing with the divided messages
+    processes = list()
+    for index in range(n_process):
+        x = Process(target=mp_function, args=(ptss, index, msgs[index], tf))
+        processes.append(x)
+        x.start()
+        
+    for process in processes:
+        process.join()
+    # Reassemble the lists of points into a sinle list of points
+    #print("msgs =", msgs[0][0:20])
+    pts = [pt for points in ptss for pt in points]
+    print pts[0]
     return pts
 
 
@@ -288,11 +339,13 @@ class RosNode:
             if msg.data == []:              # if message holds no data
                 pass                        # do nothing
             else:
-                (trans, quat) = self.get_tf()                                   # Obtain the proper transform                         
-                points = pc2_to_pts(msg)                                        # Extract point info from message data (30->11.5)
+                #print("original = ", msg.data[0:20])
+                tf = self.get_tf()                                              # Obtain the proper transform                         
+                points = pc2_multiprocess(1, msg, tf)                 # Transform position data (multiproceess) (16->??)
+                #points = pc2_to_pts(msg)                                        # Extract point info from message data (30->11.5)
                 #points = transform_points(trans, quat, points)                  # Transform position data (11.5->1.5)
                 #points = pts_tf_threader(4, trans, quat, points)                # Transform position data (threaded) (11.5->1.1)
-                points = pts_tf_multiprocess(cpu_count(), trans, quat, points)  # Transform position data (multiproceess) (16->3.6)
+                #points = pts_tf_multiprocess(cpu_count(), trans, quat, points)  # Transform position data (multiproceess) (16->3.6)
                 #self.writePoints(points)                                        # Write points to file(2->1.6) 25%
                 self.pub.publish(Empty())                                       # Pub when data is_recorded (to check hz)
 
